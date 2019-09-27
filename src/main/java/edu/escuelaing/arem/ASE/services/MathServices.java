10f43756d0d13d1f65b743cd59f0d2396c85e107
nontoa <nicolasnc010@gmail.com>
2019-09-27 11:57:11 -0500

    Inicializando

---
+++ src/main/java/edu/escuelaing/arem/ASE/services/MathServices.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem.ASE.services;
+
+/**
+ *
+ * @author 2137516
+ */
+public class MathServices {
+    
+    public static Integer square(Integer i){
+        return i*i;
+    }
+}
